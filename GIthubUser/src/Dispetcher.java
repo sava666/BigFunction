bb31dff71dc8da60c416fe841ad2dcc6ca6832af
sava666 <saviak.nazar@gmail.com>
2017-12-22 07:53:41 +0200

    user

--- GIthubUser/src/Dispetcher.java
+++ GIthubUser/src/Dispetcher.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 
 public class Dispetcher {
-    public static float ser(int a, int b){
-        return (a + b) / 2.0;
-    }
+
+	public static void main(String[] args) {
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Enter number a:");
+		int a = reader.nextInt(); // Scans the next token of the input as an int.
+		System.out.println("Enter number b:");
+		int b = reader.nextInt(); // Scans the next token of the input as an int.
+		//once finished
+		reader.close(); 
+
+	}
+
 }
